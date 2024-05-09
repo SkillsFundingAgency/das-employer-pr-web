94417a3ad7605e25e15a0e8b49d19b8518272f76
Deven Shah <devenshah1974@gmail.com>
2024-05-09 08:34:49 +0100

    CSP-1337 Removed comments

--- src/SFA.DAS.Employer.PR.Web/Authentication/PolicyNames.cs
+++ src/SFA.DAS.Employer.PR.Web/Authentication/PolicyNames.cs
@@ -1,6 +1,5 @@
 ﻿namespace SFA.DAS.Employer.PR.Web.Authentication;
 
-//MFCMFC no idea if the 'HasEmployerAccount' should be here
 public static class PolicyNames
 {
     public static string IsAuthenticated => nameof(IsAuthenticated);
--- src/SFA.DAS.Employer.PR.Web/Infrastructure/RouteValueKeys.cs
+++ src/SFA.DAS.Employer.PR.Web/Infrastructure/RouteValueKeys.cs
@@ -2,6 +2,5 @@
 
 public static class RouteValueKeys
 {
-    // MFCMFC this value is lifted from Employer AAN web, no idea if it's the one to use
     public const string EncodedAccountId = "employerAccountId";
 }
