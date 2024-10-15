15339f6cb60ff1252a1eee4f5f8eaafe6ca15407
Deven Shah <devenshah1974@gmail.com>
2024-10-15 16:42:13 +0100

    CSP-1509 Validated current users email with email in request (#31)
    * CSP-1509 Validated current users email with email in request
    
    * CSP-1509 Hide menu on shutter pages when coming via requests endpoint

--- src/SFA.DAS.Employer.PR.Web.UnitTests/Controllers/RequestsControllerTests/RequestsControllerGetRequestDetailsTests.cs
+++ src/SFA.DAS.Employer.PR.Web.UnitTests/Controllers/RequestsControllerTests/RequestsControllerGetRequestDetailsTests.cs
@@ -5,7 +5,6 @@ using SFA.DAS.Employer.PR.Domain.Models;
 using SFA.DAS.Employer.PR.Domain.OuterApi.Responses;
 using SFA.DAS.Employer.PR.Web.Constants;
 using SFA.DAS.Employer.PR.Web.Controllers;
-using SFA.DAS.Employer.PR.Web.Infrastructure;
 using SFA.DAS.Employer.PR.Web.Models.Requests;
 using SFA.DAS.Employer.PR.Web.UnitTests.TestHelpers;
 using SFA.DAS.Employer.Shared.UI;
@@ -13,10 +12,10 @@ using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.Employer.PR.Web.UnitTests.Controllers.RequestsControllerTests;
 
-public class RequestsControllerValidateTests
+public class RequestsControllerGetRequestDetailsTests
 {
     [Test, MoqAutoData]
-    public async Task Validate_SetsAccountTaskInContextToHideMenus(
+    public async Task GetRequestDetails_InvalidRequest_ReturnsPageNotFound(
         [Frozen] Mock<IOuterApiClient> outerApiClientMock,
         [Greedy] RequestsController sut,
         Guid requestId,
@@ -24,31 +23,16 @@ public class RequestsControllerValidateTests
     {
         ValidateCreateAccountRequestResponse response = new() { IsRequestValid = false };
         outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
+
         sut.AddDefaultContext();
 
-        await sut.ValidateCreateAccountRequest(requestId, cancellationToken);
-
-        sut.ControllerContext.HttpContext.Items.Should().ContainKey(SessionKeys.AccountTasksKey);
-    }
-
-    [Test, MoqAutoData]
-    public async Task Validate_InvalidRequest_ReturnsPageNotFound(
-        [Frozen] Mock<IOuterApiClient> outerApiClientMock,
-        [Greedy] RequestsController sut,
-        Guid requestId,
-        CancellationToken cancellationToken)
-    {
-        ValidateCreateAccountRequestResponse response = new() { IsRequestValid = false };
-        outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
-        sut.AddDefaultContext();
-
-        var result = await sut.ValidateCreateAccountRequest(requestId, cancellationToken);
+        var result = await sut.GetRequestDetails(requestId, cancellationToken);
 
         result.As<ViewResult>().ViewName.Should().Be(RequestsController.PageNotFoundViewPath);
     }
 
     [Test, MoqAutoData]
-    public async Task Validate_InvalidRequestStatus_ReturnsInvalidRequestStatusShutterPage(
+    public async Task GetRequestDetails_InvalidRequestStatus_ReturnsInvalidRequestStatusShutterPage(
         [Frozen] Mock<IOuterApiClient> outerApiClientMock,
         [Frozen] UrlBuilder builder,
         [Greedy] RequestsController sut,
@@ -62,16 +46,17 @@ public class RequestsControllerValidateTests
             HasValidaPaye = true
         };
         outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
+
         sut.AddDefaultContext();
 
-        var result = await sut.ValidateCreateAccountRequest(requestId, cancellationToken);
+        var result = await sut.GetRequestDetails(requestId, cancellationToken);
 
         result.As<ViewResult>().ViewName.Should().Be(RequestsController.InvalidRequestStatusShutterPageViewPath);
         result.As<ViewResult>().Model.As<InvalidCreateAccountRequestShutterPageViewModel>().AccountsUrl.Should().Be(builder.AccountsLink());
     }
 
     [Test, MoqAutoData]
-    public async Task Validate_InvalidPayeDetails_ReturnsInvalidRequestStatusShutterPage(
+    public async Task GetRequestDetails_InvalidPayeDetails_ReturnsInvalidRequestStatusShutterPage(
         [Frozen] Mock<IOuterApiClient> outerApiClientMock,
         [Frozen] UrlBuilder builder,
         [Greedy] RequestsController sut,
@@ -85,16 +70,17 @@ public class RequestsControllerValidateTests
             HasValidaPaye = false
         };
         outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
+
         sut.AddDefaultContext();
 
-        var result = await sut.ValidateCreateAccountRequest(requestId, cancellationToken);
+        var result = await sut.GetRequestDetails(requestId, cancellationToken);
 
         result.As<ViewResult>().ViewName.Should().Be(RequestsController.InvalidRequestStatusShutterPageViewPath);
         result.As<ViewResult>().Model.As<InvalidCreateAccountRequestShutterPageViewModel>().AccountsUrl.Should().Be(builder.AccountsLink());
     }
 
     [Test, MoqAutoData]
-    public async Task Validate_EmployerAccountAlreadyExists_ReturnsAccountAlreadyExistsShutterPage(
+    public async Task GetRequestDetails_EmployerAccountAlreadyExists_ReturnsAccountAlreadyExistsShutterPage(
         [Frozen] Mock<IOuterApiClient> outerApiClientMock,
         [Frozen] UrlBuilder builder,
         [Greedy] RequestsController sut,
@@ -109,20 +95,23 @@ public class RequestsControllerValidateTests
             HasEmployerAccount = true
         };
         outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
+
         sut.AddDefaultContext();
 
-        var result = await sut.ValidateCreateAccountRequest(requestId, cancellationToken);
+        var result = await sut.GetRequestDetails(requestId, cancellationToken);
 
         result.As<ViewResult>().ViewName.Should().Be(RequestsController.AccountAlreadyExistsShutterPageViewPath);
         result.As<ViewResult>().Model.As<InvalidCreateAccountRequestShutterPageViewModel>().AccountsUrl.Should().Be(builder.AccountsLink());
     }
 
     [Test, MoqAutoData]
-    public async Task Validate_RequestIsValid_RedirectsToCreateAccountCheckDetails(
+    public async Task GetRequestDetails_UserEmailAndRequestEmailDoNotMatch_RedirectsToCreateAccountCheckDetails(
         [Frozen] Mock<IOuterApiClient> outerApiClientMock,
         [Frozen] UrlBuilder builder,
         [Greedy] RequestsController sut,
         Guid requestId,
+        GetPermissionRequestResponse permissionRequest,
+        string email,
         CancellationToken cancellationToken)
     {
         ValidateCreateAccountRequestResponse response = new()
@@ -133,10 +122,64 @@ public class RequestsControllerValidateTests
             HasEmployerAccount = false
         };
         outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
-        sut.AddDefaultContext();
+        outerApiClientMock.Setup(o => o.GetPermissionRequest(requestId, cancellationToken)).ReturnsAsync(permissionRequest);
 
-        var result = await sut.ValidateCreateAccountRequest(requestId, cancellationToken);
+        sut.AddDefaultContext(email);
 
-        result.As<RedirectToRouteResult>().RouteName.Should().Be(RouteNames.CreateAccountCheckDetails);
+        var result = await sut.GetRequestDetails(requestId, cancellationToken);
+
+        result.As<ViewResult>().ViewName.Should().Be(RequestsController.UserEmailDoesNotMatchRequestShutterPageViewPath);
+    }
+
+    [Test, MoqAutoData]
+    public async Task GetRequestDetails_ValidRequests_ReturnsCheckDetailsView(
+        [Frozen] Mock<IOuterApiClient> outerApiClientMock,
+        [Frozen] UrlBuilder builder,
+        [Greedy] RequestsController sut,
+        Guid requestId,
+        GetPermissionRequestResponse permissionRequest,
+        CancellationToken cancellationToken)
+    {
+        ValidateCreateAccountRequestResponse response = new()
+        {
+            IsRequestValid = true,
+            Status = RequestStatus.Sent,
+            HasValidaPaye = true,
+            HasEmployerAccount = false
+        };
+        outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
+        outerApiClientMock.Setup(o => o.GetPermissionRequest(requestId, cancellationToken)).ReturnsAsync(permissionRequest);
+
+        sut.AddDefaultContext(permissionRequest.EmployerContactEmail!);
+
+        var result = await sut.GetRequestDetails(requestId, cancellationToken);
+
+        result.As<ViewResult>().ViewName.Should().Be(RequestsController.RequestsCheckDetailsViewPath);
+    }
+
+    [Test, MoqAutoData]
+    public async Task GetRequestDetails_ValidRequests_SetsAccountTaskInContextToHideMenus(
+        [Frozen] Mock<IOuterApiClient> outerApiClientMock,
+        [Frozen] UrlBuilder builder,
+        [Greedy] RequestsController sut,
+        Guid requestId,
+        GetPermissionRequestResponse permissionRequest,
+        CancellationToken cancellationToken)
+    {
+        ValidateCreateAccountRequestResponse response = new()
+        {
+            IsRequestValid = true,
+            Status = RequestStatus.Sent,
+            HasValidaPaye = true,
+            HasEmployerAccount = false,
+        };
+        outerApiClientMock.Setup(o => o.ValidateCreateAccountRequest(requestId, cancellationToken)).ReturnsAsync(response);
+        outerApiClientMock.Setup(o => o.GetPermissionRequest(requestId, cancellationToken)).ReturnsAsync(permissionRequest);
+
+        sut.AddDefaultContext(permissionRequest.EmployerContactEmail!);
+
+        await sut.GetRequestDetails(requestId, cancellationToken);
+
+        sut.ControllerContext.HttpContext.Items.Should().ContainKey(SessionKeys.AccountTasksKey);
     }
 }
