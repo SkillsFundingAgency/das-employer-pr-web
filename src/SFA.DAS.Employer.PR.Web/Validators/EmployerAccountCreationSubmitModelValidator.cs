bec1aa0a8582327b294036d31bb2df8fc14b0f40
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-10-25 10:01:24 +0100

    Csp 1487 employer account creation page (#32)
    * CSP-1487 employer account creation page
    
    * Add grey box
    
    * Change BG colour of checkboxes
    
    * CSP-1487 minor fix to page
    
    * CSP-1487 minor fixes to text
    
    ---------
    
    Co-authored-by: James King <hello@jamesmichaelking.com>

---
+++ src/SFA.DAS.Employer.PR.Web/Validators/EmployerAccountCreationSubmitModelValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using SFA.DAS.Employer.PR.Web.Models.Requests;
+
+namespace SFA.DAS.Employer.PR.Web.Validators;
+
+public class EmployerAccountCreationSubmitModelValidator : AbstractValidator<EmployerAccountCreationSubmitModel>
+{
+    public const string AcceptAgreementErrorMessage = "You must accept the employer agreement";
+
+    public EmployerAccountCreationSubmitModelValidator()
+    {
+        RuleFor(s => s.HasAcceptedTerms)
+            .Cascade(CascadeMode.Stop)
+            .NotEqual(false)
+            .WithMessage(AcceptAgreementErrorMessage);
+    }
+}
