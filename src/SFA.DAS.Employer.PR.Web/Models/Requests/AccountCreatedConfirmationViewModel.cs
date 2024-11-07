de129609920fa768708e5a33d6bb7362261a6801
Deven Shah <devenshah1974@gmail.com>
2024-11-07 18:06:00 +0000

    CSP-1731 Added new confirmation view

---
+++ src/SFA.DAS.Employer.PR.Web/Models/Requests/AccountCreatedConfirmationViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.Employer.PR.Web.Models.Requests;
+
+public record AccountCreatedConfirmationViewModel(string AccountsHomeUrl, string ProviderName, string ViewAgreementUrl, string ManageProvidersUrl)
+{
+    public string ProviderNameInUpperCase => ProviderName.ToUpper();
+}
+
